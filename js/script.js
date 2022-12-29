4faae0414f6525dc7516d1aa676f728d653ad227
viktoriia-yash <yashkina.vic@gmail.com>
2022-12-29 23:53:56 +0100

    added onclick for numpad

--- js/script.js
+++ js/script.js
@@ -19,3 +19,70 @@ var btn_multiply = document.querySelector("#multiply");
 var btn_divide = document.querySelector("#divide");
 var btn_point = document.querySelector("#point");
 var btn_percentage = document.querySelector("#percentage");
+
+var numbers = document.querySelector("#numbers");
+var result = document.querySelector("#result");
+
+btn_zero.onclick = () => {
+  type_to(0);
+};
+
+btn_one.onclick = () => {
+  type_to(1);
+};
+
+btn_two.onclick = () => {
+  type_to(2);
+};
+
+btn_three.onclick = () => {
+  type_to(3);
+};
+
+btn_four.onclick = () => {
+  type_to(4);
+};
+
+btn_five.onclick = () => {
+  type_to(5);
+};
+
+btn_six.onclick = () => {
+  type_to(6);
+};
+
+btn_seven.onclick = () => {
+  type_to(7);
+};
+
+btn_eight.onclick = () => {
+  type_to(8);
+};
+
+btn_nine.onclick = () => {
+  type_to(9);
+};
+
+btn_plus.onclick = () => {
+  type_to("+");
+};
+
+btn_minus.onclick = () => {
+  type_to("-");
+};
+
+btn_multiply.onclick = () => {
+  type_to("*");
+};
+
+btn_divide.onclick = () => {
+  type_to("/");
+};
+
+btn_point.onclick = () => {
+  type_to(".");
+};
+
+btn_percentage.onclick = () => {
+  type_to("%");
+};
