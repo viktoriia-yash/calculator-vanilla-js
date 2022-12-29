00f6dbc71d853bc0c7f1485e2e59da6f57a058f2
viktoriia-yash <yashkina.vic@gmail.com>
2022-12-29 18:00:03 +0100

    Chose buttons in the JS file, changed screen height in the CSS file
    Also fixed problems caused of renaming elements

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var erase = document.querySelector("#erase");
+var clear = document.querySelector("#clear");
+var equal = document.querySelector("#answer");
+
+var btn_zero = document.querySelector("zero");
+var btn_one = document.querySelector("one");
+var btn_two = document.querySelector("two");
+var btn_three = document.querySelector("three");
+var btn_four = document.querySelector("four");
+var btn_five = document.querySelector("five");
+var btn_six = document.querySelector("six");
+var btn_seven = document.querySelector("seven");
+var btn_eight = document.querySelector("eight");
+var btn_nine = document.querySelector("nine");
+
+var btn_plus = document.querySelector("#plus");
+var btn_minus = document.querySelector("#minus");
+var btn_multiply = document.querySelector("#multiply");
+var btn_divide = document.querySelector("#divide");
+var btn_point = document.querySelector("#point");
+var btn_percentage = document.querySelector("#percentage");
